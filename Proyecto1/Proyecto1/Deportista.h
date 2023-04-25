6b08e3d0b3bb42f033c4dd16437595b30628ffec
Kristel <Samanthaduarte0504@gmail.com>
2023-04-25 00:50:56 -0600

    set y get basicos en deportista

---
+++ Proyecto1/Proyecto1/Deportista.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Deportista // : public Triatlonista
+{
+public:
+	Deportista(string, string, int, char);
+	virtual ~Deportista();
+
+	//set y get
+	virtual string getId();
+	virtual void setId(string);
+	virtual string getNombre();
+	virtual void setNombre(string);
+	virtual int getTelefono();
+	virtual void setTelefono(int);
+	virtual char getSexo();
+	virtual void setSexo(char);
+
+	//Hacer metodo para obtener fecha de nacimiento por día, mes y año.
+	//Pedir y obtener datos biométricos.
+	//pedir fecha de registro
+
+private:
+	string id;
+	string nombre;
+	int telefono;
+	char sexo;
+};
