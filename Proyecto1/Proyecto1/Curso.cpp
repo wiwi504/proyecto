f3fea469949d9f9b363fc248f8d2a980df6d3613
Kristel <Samanthaduarte0504@gmail.com>
2023-04-28 16:40:48 -0600

    casi nada de cambios en curso

--- Proyecto1/Proyecto1/Curso.cpp
+++ Proyecto1/Proyecto1/Curso.cpp
@@ -2,7 +2,7 @@
 #include <sstream>
 using std::stringstream;
 
-Curso::Curso(string)
+Curso::Curso(string nombre) : nombre(nombre) //no sé cómo inicializar la plantilla
 {
 }
 
@@ -10,8 +10,9 @@ Curso::~Curso()
 {
 }
 
-void Curso::agregarGrupo(Lista<Grupo>*)
+void Curso::agregarGrupo(Lista<Grupo>* grupo)
 {
+
 }
 
 string Curso::toString()
